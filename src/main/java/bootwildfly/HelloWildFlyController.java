44635f9ce02b69b7cbbc7de49468cfa6b751b505
Andrew Alsup <bluesbreaker@gmail.com>
2016-06-12 09:59:37 -0400

    Added support for CUSTOM_LOCATION env

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -8,7 +8,8 @@ public class HelloWildFlyController {
 
 
     @RequestMapping("hello")
-    public String sayHello(){
-        return ("Hello, SpringBoot on Wildfly");
+    public String sayHello() {
+        String location = (System.getenv("CUSTOM_LOCATION") == null) ? "Maryland" : System.getenv("CUSTOM_LOCATION");
+        return ("Hello from " + location);
     }
 }
