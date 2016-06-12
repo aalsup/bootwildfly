318ea254bc244f08e12a3c504672f57f4dc35d16
Andrew Alsup <bluesbreaker@gmail.com>
2016-06-12 11:33:29 -0400

    Added server IP/hostname info

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -1,18 +1,26 @@
 package bootwildfly;
 
+import java.net.InetAddress;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWildFlyController {
 
-
     @RequestMapping("hello")
     public String sayHello() {
+        // Get server IP/host info
+        InetAddress inet = InetAddress.getLocalHost();
+        String ip = inet.getHostAddress().toString();
+        String hostname = inet.getHostName();
+        String serverInfo =  hostname + "/" + ip;
+
+        // Get location info from ENV
         String location = System.getenv("CUSTOM_LOCATION");
         if ((location == null) || (location.length() == 0)) {
             location = "Maryland";
         }
-        return ("Goodbye from " + location);
+
+        return ("Hello from " + location + " on server " + serverInfo);
     }
 }
